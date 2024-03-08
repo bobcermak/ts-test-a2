3d875c61291bea6a92afa13fd0bff69a0ec6eed4
bobcermak <145228912+bobcermak@users.noreply.github.com>
2024-03-08 19:07:58 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,45 @@
+
+type Hodnoceni = {
+    jmeno: string,
+    znamka: number
+}
+// Máte seznam jmen a známek studentů ve třídě. 
+// Například: 
+let znamky: Array<Hodnoceni> = [
+    { jmeno: "Petr", znamka: 2 },
+    { jmeno: "Anna", znamka: 1 },
+    { jmeno: "Jana", znamka: 3 },
+    { jmeno: "David", znamka: 1 }
+]
+/* 
+Napište algoritmus, který vytvoří nový seznam 
+obsahující pouze jména studentů s nejlepší známkou. 
+Pokud je více studentů se stejnou nejlepší známkou, 
+zahrňte je všechny do seznamu.
+*/
+
+let bestZnamka = 5
+const novySeznam: Array<string> = []; //Například: ["Anna", "David"].
+
+
+for (let i of znamky) {
+    if (i.znamka <= bestZnamka) {
+        bestZnamka = i.znamka
+    }
+}
+
+for (let i of znamky) {
+    if (i.znamka === bestZnamka) {
+        novySeznam.push(i.jmeno)
+    }
+}
+
+
+
+
+
+
+
+
+
+console.logValue("šprti: ", novySeznam)
